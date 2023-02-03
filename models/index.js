97a691f98d93096099da0b929dd8428ba627dc4f
wdiste <Wdiste49@gmail.com>
2023-02-03 17:22:55 -0500

    source code added and filled out productTag  & tag  models according to readme as well as filled in index.js model

--- models/index.js
+++ models/index.js
@@ -1,16 +1,22 @@
-// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Category.hasOne(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
-// Products belongToMany Tags (through ProductTag)
+ProductTag = sequelize.define('product_tag', {});
 
-// Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag });
+Tag.belongsToMany(Product, { through: ProductTag });
 
 module.exports = {
   Product,
